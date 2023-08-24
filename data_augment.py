8633023923804ee4c6b8263df7c6ea451ab61ece
Zeynep Seda Birinci <zseda@gmail.com>
2023-08-24 11:39:12 +0200

    pool for parallel processing

--- data_augment.py
+++ data_augment.py
@@ -3,7 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool
 from pathlib import Path
 import wandb
 
@@ -42,34 +42,22 @@ dataloader_augmented = DataLoader(dataset_augmented, batch_size=32, shuffle=True
 dataset_original = CIFAR10(root="./data", train=True, download=True, transform=transform_original)
 dataloader_original = DataLoader(dataset_original, batch_size=32, shuffle=True)
 
-# Initialize lists to hold batch of images
-original_images_batch = []
-augmented_images_batch = []
-
 # Function to save and log image
-def save_and_log_image(image, image_augmented, i, j):
+def save_and_log_image(args):
+    image, image_augmented, i, j = args
     image_path = save_dir / f"{i}_{j}.png"
     transforms.ToPILImage()(image_augmented).save(image_path)
-    
-    # Add to batch
-    original_images_batch.append(wandb.Image(transforms.ToPILImage()(image), caption=f"Original {i}_{j}"))
-    augmented_images_batch.append(wandb.Image(image_path, caption=f"Augmented {i}_{j}"))
-
-    # Log if batch size is reached
-    if len(original_images_batch) >= 10:  # Adjust batch size as needed
-        wandb.log({
-            "Original Images": original_images_batch,
-            "Augmented Images": augmented_images_batch
-        })
-        # Clear the batches
-        original_images_batch.clear()
-        augmented_images_batch.clear()
+    wandb.log({
+        "Image Comparisons": [wandb.Image([transforms.ToPILImage()(image), image_path], caption=f"Comparison {i}_{j}")]
+    })
 
 # Save augmented images and log to wandb
-with ThreadPoolExecutor() as executor:
+with Pool() as pool:
+    args_list = []
     for i, ((images, _), (images_augmented, _)) in enumerate(zip(dataloader_original, dataloader_augmented)):
         for j, (image, image_augmented) in enumerate(zip(images, images_augmented)):
-            executor.submit(save_and_log_image, image, image_augmented, i, j)
+            args_list.append((image, image_augmented, i, j))
+    pool.map(save_and_log_image, args_list)
 
 # Log augmented images to wandb
 wandb.save(str(save_dir / "*"))
