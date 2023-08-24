992d45a3c2c7d18a2d8e560a197946c5e70cc4e8
Zeynep Seda Birinci <zseda@gmail.com>
2023-08-24 12:46:12 +0200

    bug fix

--- data_augment.py
+++ data_augment.py
@@ -2,10 +2,9 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
-from torch.utils.data import DataLoader, RandomSampler
+from torch.utils.data import DataLoader
 from pathlib import Path
 import wandb
-import numpy as np
 
 # Initialize wandb
 wandb.init(project="cifar10_autoaugment")
@@ -34,32 +33,38 @@ transform_original = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-# Set random seed for reproducibility
-random_seed = 42
-
 # Load CIFAR-10 dataset with augmented transforms
 dataset_augmented = CIFAR10(root="./data", train=True, download=True, transform=transform_augmented)
-dataloader_augmented = DataLoader(dataset_augmented, batch_size=10, sampler=RandomSampler(dataset_augmented, replacement=False, num_samples=None, generator=torch.Generator().manual_seed(random_seed)))
+dataloader_augmented = DataLoader(dataset_augmented, batch_size=10, shuffle=False)
 
 # Load CIFAR-10 dataset with original transforms
 dataset_original = CIFAR10(root="./data", train=True, download=True, transform=transform_original)
-dataloader_original = DataLoader(dataset_original, batch_size=10, sampler=RandomSampler(dataset_original, replacement=False, num_samples=None, generator=torch.Generator().manual_seed(random_seed)))
+dataloader_original = DataLoader(dataset_original, batch_size=10, shuffle=False)
 
 # Function to save and log image
 def save_and_log_image(image, image_augmented, i, j):
-    image_path = save_dir / f"{i}_{j}.png"
+    image_path = save_dir / f"{i*10 + j}.png"
     transforms.ToPILImage()(image_augmented).save(image_path)
-    wandb.log({
-        "Original Images": [wandb.Image(transforms.ToPILImage()(image), caption=f"Original {i}_{j}")],
-        "Augmented Images": [wandb.Image(transforms.ToPILImage()(image_augmented), caption=f"Augmented {i}_{j}")]
-    })
+
+# Initialize lists to hold wandb.Image objects
+original_images = []
+augmented_images = []
 
 # Save augmented images and log to wandb
 for i, ((images, _), (images_augmented, _)) in enumerate(zip(dataloader_original, dataloader_augmented)):
     for j, (image, image_augmented) in enumerate(zip(images, images_augmented)):
         save_and_log_image(image, image_augmented, i, j)
-    if i == 9:  # Log only the first 100 images (10 batches of 10)
-        break
+        if i % 10 == 0:  # Log every 10 batches
+            original_images.append(wandb.Image(transforms.ToPILImage()(image), caption=f"Original {i*10 + j}"))
+            augmented_images.append(wandb.Image(transforms.ToPILImage()(image_augmented), caption=f"Augmented {i*10 + j}"))
+    
+    if i % 10 == 0:  # Log every 10 batches
+        wandb.log({
+            "Original Images": original_images,
+            "Augmented Images": augmented_images
+        })
+        original_images = []
+        augmented_images = []
 
 # Log augmented images to wandb
 wandb.save(str(save_dir / "*"))
